02ac8692c953ce84d5d7199e79f068014ab3142d
TSITCL\s152119 <suzukikeita.trident@gmail.com>
2017-04-10 14:51:16 +0900

    リザルト画面

--- SharkGame/Classes/ResultScene.cpp
+++ SharkGame/Classes/ResultScene.cpp
@@ -1,8 +1,9 @@
 #include "TitleScene.h"
 #include "ResultScene.h"
-#include "AudioEngine.h"
+#include "audio/include/AudioEngine.h"
 
 USING_NS_CC;
+
 
 using namespace experimental;
 
@@ -31,32 +32,85 @@ bool ResultScene::init()
 		// 基底クラスの初期化が失敗なら、異常終了
 		return false;
 	}
+
+	//音の取得
+	int resultbgm = AudioEngine::play2d("resultbgm.mp3");
+
+	AudioEngine::setLoop(resultbgm, true);
+
+
+	//画面の縦横のサイズを取得する
 	Size winSize = Director::getInstance()->getWinSize();
 	Point origin = Director::getInstance()->getVisibleOrigin();
 
-	auto background = Sprite::create("gameover.png");
+	//背景の画像を取得
+	Sprite* background = Sprite::create("gameover.png");
 
+	//背景の画像を画面中央に表示
 	background->setPosition(Point(winSize.width / 2, winSize.height / 2));
+	//background->setPosition((Vec2(480, 320)));
 
-	this->addChild(background,0);
+	//画像のサイズの調整
+	background->setScale(1.4f);
+
+	addChild(background,0);
+
+	//スタートボタン画像
+	ui::Button* resulttitle = ui::Button::create("Restart-button.png");
+	resulttitle->setPosition(Point(480, 200));
+	resulttitle->setScale(1.0f);
+	this->addChild(resulttitle);
 
 	// 毎フレーム更新を有効化
 	scheduleUpdate();
+
+	EventListenerTouchOneByOne* listener = EventListenerTouchOneByOne::create();
+	listener->onTouchBegan = CC_CALLBACK_2(ResultScene::onTouchBegan, this);
+	_director->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
+
+	resulttitle->addTouchEventListener(CC_CALLBACK_2(ResultScene::onButtonClick, this));
 
 	// 初期化が正常終了
 	return true;
 }
 
-// 毎フレーム更新
+// タッチ開始時コールバック
+bool ResultScene::onTouchBegan(Touch* touch, Event* pEvent)
+{
+	return false;
+}
+
+
+//自動でシーンを変更の場合（消えるバグ2回に１回の割合あり）
 void ResultScene::update(float delta)
 {
-	
-	Scene* nextScene = TitleScene::create();
+	////リザルトシーンからタイトルシーンへ//
+	//Scene* nextScene = TitleScene::create();
 
-	TransitionFade* fade = TransitionFade::create(2.0f, nextScene, Color3B::BLACK);
+	////リザルトシーンから2秒たったらブラックアウトしてタイトルシーンへ戻る
+	//TransitionFade* fade = TransitionFade::create(10.0f, ResultScene, Color3B::BLACK);
 	
-	// 次のシーンに移行
+	//// 次のシーンに移行
 	//_director->replaceScene(nextScene);
-	Director::getInstance()->replaceScene(fade);
+	
+	////今のシーンを削除
+	//Director::getInstance()->replaceScene(fade);
 
+}
+
+////ボタンを押してシーン変更
+void ResultScene::onButtonClick(Ref* ref, cocos2d::ui::Widget::TouchEventType eventType)
+{
+	if (eventType == ui::Widget::TouchEventType::ENDED)
+	{
+		// 次のシーンを作成する
+		Scene* nextScene = TitleScene::create();
+
+		// フェードアウトトランジション
+		//nextScene = TransitionFade::create(1.0f, nextScene, Color3B(255, 255, 0));
+		//nextScene = TransitionOriginal::create(1.0f, nextScene);
+
+		// 次のシーンに移行
+		_director->replaceScene(nextScene);
+	}
 }
--- SharkGame/Classes/ResultScene.h
+++ SharkGame/Classes/ResultScene.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
 
 // タイトルシーン
 class ResultScene : public cocos2d::Scene
 {
+private:
+	
+	cocos2d::Sprite* background;
+
 public:
 	// create関数の宣言と定義
 	//CREATE_FUNC(StaffRollScene);
@@ -13,4 +18,13 @@ public:
 	bool init();
 
 	void update(float delta) override;
+
+	bool onTouchBegan(cocos2d::Touch * touch, cocos2d::Event * pEvent);
+	void onButtonClick(Ref* ref ,cocos2d::ui::Widget::TouchEventType eventType);
+	cocos2d::ui::Button* button; //ボタン
+
+	cocos2d::ui::Button* resulttitle = nullptr;  //EXitボタン
+
+	
+	cocos2d::Sprite* sprite = nullptr;
 };
